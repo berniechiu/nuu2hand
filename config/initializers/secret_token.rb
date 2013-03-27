e033d8c3b7cc4cf95d0e46c65be37752e3cd913a
Bernie Chiu <bernie_chiu@hotmail.com>
2013-03-27 15:08:54 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuu2hand::Application.config.secret_token = 'b797e2a8bac47a1bf0b8b1422afcfa5f3061426c03cc7207b61fdc89f98bc6529378023ab3bda44816dac8ccdc21462201868ba386129bd4da856301dba17c5c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nuu2hand::Application.config.session_store :cookie_store, key: '_nuu2hand_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nuu2hand::Application.config.session_store :active_record_store
