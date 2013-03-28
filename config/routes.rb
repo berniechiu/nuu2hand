6bb1e3524955c60d225875976c3af7a24b896262
Bernie Chiu <bernie_chiu@hotmail.com>
2013-03-28 21:12:20 +0800

    Basic layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Nuu2hand::Application.routes.draw do  # The priority is based upon order of creation:
+  
+  get 'users/new'
+
   root to: 'static_pages#home'
 
-  match '/about', to: 'static_pages#about'
+  match '/signup',  to: 'users#new'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # first created -> highest priority.
 
