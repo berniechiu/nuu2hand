edca19868ef320f03ec15e778b4c650270c24a4d
Bernie Chiu <bernie_chiu@hotmail.com>
2013-04-07 13:19:49 +0800

    Change default book photo path

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,9 @@ class Book < ActiveRecord::Base
   attr_accessible :condition, :notes, :title, :photo
 
   belongs_to :user
-  has_attached_file :photo
+  has_attached_file :photo,
+  	url:  "/assets/books/:id/:style/:basename.:extension",
+  	path: ":rails_root/public/assets/books/:id/:style/:basename.:extension"
 
   validates :condition, presence: true, length: { maximum: 50 }
   validates :notes, presence: true, length: { maximum: 140 }
